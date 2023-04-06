b8caf35d1f05a9bab5e9e81ae84f51b97558c68f
Ingvar <ingvar.soloma@gmail.com>
2023-04-07 01:08:44 +0300

    IDE helper

--- app/Models/Recipe.php
+++ app/Models/Recipe.php
@@ -2,10 +2,44 @@
 
 namespace App\Models;
 
+use Database\Factories\RecipeFactory;
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Support\Carbon;
 
+/**
+ * App\Models\Recipe
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property string $title
+ * @property string $description
+ * @property string $ingredients
+ * @property string $instructions
+ * @property string $image
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property-read Collection<int, Category> $categories
+ * @property-read int|null $categories_count
+ * @method static RecipeFactory factory($count = null, $state = [])
+ * @method static Builder|Recipe newModelQuery()
+ * @method static Builder|Recipe newQuery()
+ * @method static Builder|Recipe query()
+ * @method static Builder|Recipe whereCreatedAt($value)
+ * @method static Builder|Recipe whereDescription($value)
+ * @method static Builder|Recipe whereId($value)
+ * @method static Builder|Recipe whereImage($value)
+ * @method static Builder|Recipe whereIngredients($value)
+ * @method static Builder|Recipe whereInstructions($value)
+ * @method static Builder|Recipe whereTitle($value)
+ * @method static Builder|Recipe whereUpdatedAt($value)
+ * @method static Builder|Recipe whereUserId($value)
+ * @mixin Eloquent
+ */
 class Recipe extends Model
 {
     use HasFactory;
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +12,7 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function register()
+    public function register(): void
     {
         //
     }
@@ -21,8 +22,10 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(): void
     {
-        //
+        if ($this->app->isLocal()) {
+            $this->app->register(IdeHelperServiceProvider::class);
+        }
     }
 }
