1b32818702ff110162cea678845d08369cecc47b
Ingvar <ingvar.soloma@gmail.com>
2023-04-08 14:42:51 +0300

    swagger install

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -4,11 +4,53 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use Illuminate\Http\JsonResponse;
+use OpenApi\Annotations as OA;
 
+/**
+ * @OA\Info(
+ *     title="RecipeBox API",
+ *     version="1.0.0",
+ *     description="API for managing recipes in RecipeBox"
+ * )
+ */
 class CategoryController extends Controller
 {
+
     /**
      * Get a list of all categories.
+     * @OA\Get(
+     *      path="/api/categoryList",
+     *      operationId="getCategories",
+     *      tags={"Categories"},
+     *      summary="Get a list of all categories",
+     *      description="Returns a list of all categories, sorted alphabetically by name.",
+     *      @OA\Response(
+     *          response=200,
+     *          description="Successful operation",
+     *          @OA\JsonContent(
+     *              type="array",
+     *              @OA\Items(
+     *                  type="object",
+     *                  @OA\Property(property="id", type="integer", example=1),
+     *                  @OA\Property(property="name", type="string", example="Category A")
+     *              )
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="Unauthenticated",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="message", type="string", example="Unauthenticated.")
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=403,
+     *          description="Unauthorized",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="message", type="string", example="Unauthorized.")
+     *          )
+     *      )
+     * )
      *
      * @return JsonResponse
      */
